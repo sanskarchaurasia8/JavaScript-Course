b3d87beae036c8da5d52b63a87e71b98938cc8f8
sanskarchaurasia8 <chaurasiasanskar8@gmail.com>
2025-09-03 12:00:08 +0000

    dev container

---
+++ 01_basic/datatype.js
@@ -0,0 +1,9 @@
+let string = "sanskar";
+let number = 14;
+let object = null;
+let undefined;
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof object);
+console.log(typeof undefined);
